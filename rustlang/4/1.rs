3be94628cfa8d2d3d38a17925861880e20baa0a9
Adeilson Silva <adeilsonsilva@dcc.ufba.br>
2020-03-13 10:07:06 -0300

    Adds exercise 4-1!

---
+++ rustlang/4/1.rs
@@ -0,0 +1,69 @@
+use std::io;
+
+/* Pattern to be searched for. */
+const PATTERN : &str = "ould";
+
+fn main()
+{
+    let mut input = String::new();
+
+    loop {
+        match io::stdin().read_line(&mut input) {
+            Ok (_bytes) => {
+                if _bytes == 0 {
+                    break;
+                }
+
+                let trimmed_input = input.trim_end();
+                let pos: i32 = strindex(trimmed_input, PATTERN);
+
+                print!("[*] Rightmost position of '{}' in '{}' is: '{}'\n", PATTERN, trimmed_input, pos);
+
+                // clear input so lines don't get appended
+                input.clear();
+            },
+            Err(_) => {
+                panic!("Unexpected error.");
+            }
+        }
+    }
+}
+
+/**
+ * A function to check for the rightmost occurence of t in s.
+ *
+ * @param char[] s The string to be searched at
+ * @param char[] t The pattern to be searched for
+ *
+ * @return int      Position of rightmost occurrence .
+ */
+fn strindex(s: &str, t: &str) -> i32
+{
+    let mut rightmost : i32 = -1;
+
+    for (idx, _) in s.chars().enumerate() {
+        let mut str_iter = s.chars().skip(idx+1);
+        let mut pattern_iter = t.chars();
+        let mut k = 0;
+        let pend;
+
+        loop {
+            let _sc = str_iter.next();
+            let _pc = pattern_iter.next();
+
+            if _pc == None || _sc != _pc {
+                pend = _pc;
+                break;
+            }
+
+
+            k += 1;
+        };
+
+        if k > 0 && pend == None {
+            rightmost = (idx+1) as i32;
+        }
+    }
+
+    rightmost
+}
